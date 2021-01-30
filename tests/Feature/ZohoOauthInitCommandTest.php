7604601658c122d56a0a48e447f6ef1b85d602be
njoguamos <njoguamos@icloud.com>
2021-01-31 01:49:59 +0300

    Apply fixes from StyleCI (#4)
    Co-authored-by: njoguamos <njoguamos@users.noreply.github.com>

--- tests/Feature/ZohoOauthInitCommandTest.php
+++ tests/Feature/ZohoOauthInitCommandTest.php
@@ -20,32 +20,32 @@ class ZohoOauthInitCommandTest extends TestCase
     public function it_saves_authorization_tokens_to_database_on_success_response()
     {
         $successResponse = [
-            "access_token"  => "1000.5e0cd91a9e79ae7f35d4ca73a5cce204.cfc90960373093f903e509619b9ea088",
-            "refresh_token" => "1000.69be61cb69415ec9c945b16fd7767814.54cccb3b531121ba6e82ec94ddbd2339",
-            "api_domain"    => "https://www.zohoapis.com",
-            "token_type"    => "Bearer",
-            "expires_in"    => 3600,
+            'access_token'  => '1000.5e0cd91a9e79ae7f35d4ca73a5cce204.cfc90960373093f903e509619b9ea088',
+            'refresh_token' => '1000.69be61cb69415ec9c945b16fd7767814.54cccb3b531121ba6e82ec94ddbd2339',
+            'api_domain'    => 'https://www.zohoapis.com',
+            'token_type'    => 'Bearer',
+            'expires_in'    => 3600,
         ];
 
         $this->mockASuccessfulResponse($successResponse);
 
-        $this->mockFakeErrorResponse("invalid_code");
+        $this->mockFakeErrorResponse('invalid_code');
 
         $this->artisan('zoauth:init')
             ->expectsOutput('Successfully saved authorization codes to the database.');
 
         $this->assertDatabaseHas('zoho_oauth', [
-            "access_token"  => $successResponse['access_token'],
-            "refresh_token" => $successResponse['refresh_token'],
-            "api_domain"    => $successResponse['api_domain'],
-            "token_type"    => $successResponse['token_type'],
+            'access_token'  => $successResponse['access_token'],
+            'refresh_token' => $successResponse['refresh_token'],
+            'api_domain'    => $successResponse['api_domain'],
+            'token_type'    => $successResponse['token_type'],
         ]);
     }
 
     /** @test */
     public function it_catches_invalid_code_error()
     {
-        $this->mockFakeErrorResponse("invalid_code");
+        $this->mockFakeErrorResponse('invalid_code');
 
         $this->artisan('zoauth:init')
             ->expectsOutput("Invalid Code. \n 1. The grant token has expired. Generate the access and refresh tokens before the grant token expires. \n 2. You have already used the grant token.\n 3. The refresh token to generate a new access token is wrong or revoked. Specify the correct refresh token value while refreshing an access token.");
@@ -54,15 +54,15 @@ class ZohoOauthInitCommandTest extends TestCase
     /** @test */
     public function it_catches_invalid_client_error()
     {
-        $this->mockFakeErrorResponse("invalid_client");
+        $this->mockFakeErrorResponse('invalid_client');
 
         $this->artisan('zoauth:init')
-            ->expectsOutput("You have passed an invalid Client ID or secret. Specify the correct client ID and secret.");
+            ->expectsOutput('You have passed an invalid Client ID or secret. Specify the correct client ID and secret.');
     }
 
     protected function mockFakeErrorResponse(string $error, int $status = 200)
     {
-        return Http::fake([$this->url => Http::response(["error" => $error,], $status)]);
+        return Http::fake([$this->url => Http::response(['error' => $error], $status)]);
     }
 
     protected function mockASuccessfulResponse(array $reponse)
